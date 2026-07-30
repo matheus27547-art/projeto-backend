35aa02e8473a5c08c9177b1e880861d519ca43d6
JOAO VICTOR LIMA DOS SANTOS <46123@fiecalunos.lan>
2026-07-30 19:55:15 -0300

    Setup do Projeto

---
+++ src/test/java/br/edu/fiec/helptec/HelptecApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.fiec.helptec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelptecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
