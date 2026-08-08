f158648442cb96a2b1f27bb2d07270a6057dedba
Higor Santana <higor46120@fiec.edu.br>
2026-08-07 23:19:16 -0300

    setupops higorsantana

--- src/main/java/br/edu/fiec/helptec/config/FirebaseConfig.java
+++ src/main/java/br/edu/fiec/helptec/config/FirebaseConfig.java
@@ -4,18 +4,24 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 @Configuration
 public class FirebaseConfig {
+
+    @Value("${app.firebase.config-file:serviceAccountKey.json}")
+    private String configFile;
+
     @PostConstruct
     public void initialize() {
         try {
-            FileInputStream serviceAccount =
-                    new FileInputStream("src/main/resources/serviceAccountKey.json");
+            InputStream serviceAccount = new ClassPathResource(configFile).getInputStream();
 
             FirebaseOptions options = FirebaseOptions.builder()
                     .setCredentials(GoogleCredentials.fromStream(serviceAccount))
